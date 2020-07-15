310f54e38094bfcfe17c87bb11a693f7e02c2090
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-07-16 02:56:22 +0530

    added sign-in/profile and completed manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,27 +1,47 @@
-/** @format */
-
 const User = require("../models/user");
+const { response } = require("express");
 
 module.exports.profile = function (req, res) {
-	// res.end('<h1>Users profile are running!</h1>');
-	return res.render("user_profile", {
-		user: "Divyalok",
-		title: "Tell the tale",
-	});
+    if(req.cookies.user_id) {
+        if(req.cookies.user_id == "null") {
+            console.log("You logged out! Please login again");
+            return res.redirect('/users/sign-in');
+        }
+        User.findById(req.cookies.user_id, function(err, user) {
+            if(user) {
+                return res.render('user_profile', {
+                    title: "Tell The Tale",
+                    user: user.name,
+                    email: user.email,
+                    id: user.id
+                });
+            }
+        });
+    }
+    else {
+        return res.redirect('/users/sign-in');
+    }
 };
 
 //rendering sign-up page
 module.exports.signUp = function (req, res) {
 	return res.render("user_sign_up", {
-		title: "SignUp | Tell The Tale",
+		title: "Sign Up | Tell The Tale",
 	});
 };
 
 //rendering sign-in page
 module.exports.signIn = function (req, res) {
 	return res.render("user_sign_in", {
-		title: "SignIn | Tell The Tale",
+		title: "Sign In | Tell The Tale",
 	});
+};
+
+module.exports.signOut = function(req, res) {
+    if(req.cookies.user_id) {
+        res.cookie('user_id', "null");
+    }
+    return res.redirect('/users/sign-in');
 };
 
 //get the sign-up data
@@ -53,6 +73,7 @@ module.exports.create = function (req, res) {
         }
         else
         {
+            console.log('User exists');
             return res.redirect('back');
         }
 
@@ -61,4 +82,37 @@ module.exports.create = function (req, res) {
 
 //get the login data
 module.exports.login = function (req, res) {
+    //find the user 
+    User.findOne({
+        email: req.body.username
+    }, function(err, user){
+        if(err)
+        {
+            console.log('Error in finding the user');
+            return;
+        }
+
+        //handling if the user is found
+        if(user) {
+            //handle if the password don't match
+            if(user.password != req.body.password)
+            {
+                console.log("Passwords don't match");
+                return res.redirect('back');
+            }
+            //handling successful login
+            else
+            {
+                console.log('User found!');
+                res.cookie('user_id', user.id);
+                return res.redirect('/users/profile');
+            }
+
+        }
+        else //handling if user is not found
+        {
+            console.log('User not found');
+            return res.redirect('back'); 
+        }
+    });
 };

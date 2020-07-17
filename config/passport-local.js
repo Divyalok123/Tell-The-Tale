c12399c6152521a0943ef61ded307f30d3021ab7
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-07-17 17:08:30 +0530

    setting up passport.js

---
+++ config/passport-local.js
@@ -0,0 +1,44 @@
+//requiring passport
+const passport = require('passport');
+//requiring passport-local strategy
+const LocalStrategy = require('passport-local').Strategy;
+
+//importing user
+const User = require('../models/user');
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+}, function(email, password, done){ //done is the callback reporting to passport.js
+    User.findOne({email: email}, function(err, user){
+        if(err) {
+            console.log("Error occured while finding User!");
+            return done(err); //reporting error to passport
+        }
+        if(!user || user.password != password) {
+            console.log("Invalid Username/Password!");
+            return done(null, false); //first argument (representing error)-> no error and second argument (user found or not) -> not found 
+        }
+        console.log('User found!');
+        return done(null, user); //user found.. now passing the user
+    });
+}));
+
+//serializing the user to decide the key for the cookies
+//i.e. whichever user signs in we send it's key to the cookie
+passport.serializeUser(function(user, done){
+    done(null, user);
+});
+
+//deserializing the user from the key we get from the cookie
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        if(err)
+        {
+            console.log("Error occured while deserializing the user!");
+            return done(err);
+        }
+        return done(null, user);
+    });
+});
+
+module.exports = passport;

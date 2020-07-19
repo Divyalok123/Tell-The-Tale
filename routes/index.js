60f94c3a2f6cf9df7f9f9d9373d6a1ec3921708a
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-07-20 01:26:37 +0530

    created post schema connected with db

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home_controller");
 
+
 router.get("/", homeController.home);
 router.get("/project", homeController.project);
 router.get("/about", homeController.about);
 router.use("/users", require("./users"));
+router.use("/posts", require("./posts"));
 
 module.exports = router;

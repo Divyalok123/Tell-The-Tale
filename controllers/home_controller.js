f08010ff5ee035272e7bb5d2ce32dc92d9a4df58
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-07-20 02:41:01 +0530

    showing posts with username (learnt about pre-populating)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,30 @@
+const Post = require("../models/post");
+
 module.exports.home = function(req, res){
     // console.log(req.cookies); //since cookies are coming in request
     // res.cookie('user_id', 20);//changing the value of the cookie
-    return res.render('home', {
-        title: "Tell the tale",
-        person: "Divyalok"
-    })
-};
 
-module.exports.project = function(req, res){
-    return res.end('<h1>Project page is running!</h1>');
+    // Post.find({}, function(err, posts) {
+    //     if(err) {
+    //         console.log('Error occured!');
+    //         return;
+    //     }
+    //     return res.render('home', {
+    //         title: "Tell the tale",
+    //         person: "Divyalok",
+    //         posts: posts
+    //     })
+    // });
+
+    //pre-populating
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts) {
+        return res.render('home', {
+            title: "Tell The Tale",
+            person: "Divyalok",
+            posts: posts
+        });
+    });
 };
 
 module.exports.about = function(req, res) {
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const homeController = require("../controllers/home_controller");
 
 
 router.get("/", homeController.home);
-router.get("/project", homeController.project);
 router.get("/about", homeController.about);
 router.use("/users", require("./users"));
 router.use("/posts", require("./posts"));

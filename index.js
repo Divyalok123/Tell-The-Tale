695ba66fde66d23c8763b11e1d017d37009d2d4b
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-07-19 22:50:18 +0530

    set up sass (node-sass-middleware)

--- index.js
+++ index.js
@@ -13,7 +13,15 @@ const passportLocal = require("./config/passport-local-strategy");
 //setting up mongo store
 const MongoStore = require('connect-mongo')(session); //(session) argument because we need to store the session informantion in the database 
 
+const sassMiddleware = require('node-sass-middleware');
 
+app.use(sassMiddleware({
+	src: './assets/scss',
+	dest: './assets/css',
+	debug: true, //it should be false in production mode
+	outputStyle: 'extended', //whether we want it to be single line or multiline
+	prefix: '/css' //where should the server look for css files
+})); //just before the server starts so that we can precompile it before the server starts and whenever the browser asks for it, these precompiled files will be provided 
 
 //reading post requests
 app.use(express.urlencoded({ extended: true }));
